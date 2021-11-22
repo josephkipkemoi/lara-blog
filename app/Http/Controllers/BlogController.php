baa663216456a8c8496ff3e4db5527e4b3eeae8b
jkemboe <jkemboe@gmail.com>
2021-11-22 04:38:17 -0500

    Post Blog tests and controller configuration

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Blog $blog)
+    {
+        //
+        $blog->validate($request);
+    }
+
+}
---
+++ app/Models/Blog.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title','body','author'];
+
+    protected $casts = ['title' => 'string','author' => 'string','body' => 'string'];
+
+    protected $hidden = ['updated_at'];
+
+    public function validate($request)
+    {
+        // validate request
+        $request->validate(['title' => 'required','body' => 'required']);
+
+        // store request values to database
+        return Blog::create($request->all());
+    }
+}
