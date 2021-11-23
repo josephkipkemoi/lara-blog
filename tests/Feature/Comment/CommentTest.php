7e9bbe44a546bd838c31ab19e6af418b61896396
jkemboe <jkemboe@gmail.com>
2021-11-23 06:07:51 -0500

    tests for posting comment

---
+++ tests/Feature/Comment/CommentTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CommentTest extends TestCase
+{
+
+    use WithFaker;
+
+    public function test_can_post_comment()
+    {
+        $response = $this->post('/api/comment',[
+            'comment_id' => 1,
+            'comment_body' => $this->faker->text()
+        ]);
+
+        $response->assertStatus(201);
+    }
+}
