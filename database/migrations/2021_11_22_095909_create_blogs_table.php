9830aa2e8d12b5e70deb6ae05775d35f5f226881
jkemboe <jkemboe@gmail.com>
2021-11-23 10:13:36 -0500

    Delete model instance with relationships

--- database/migrations/2021_11_22_095909_create_blogs_table.php
+++ database/migrations/2021_11_22_095909_create_blogs_table.php
@@ -15,7 +15,7 @@ class CreateBlogsTable extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('user_id')->references('id')->on('users')->default(1);
+            $table->bigInteger('user_id')->references('id')->on('users')->default(1)->onDelete('cascade');
 
             $table->string('title');
             $table->string('author')->default('guest');
