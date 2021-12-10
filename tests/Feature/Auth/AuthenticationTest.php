018afabf553e0934f04283ccce51ff3edffe5827
jkemboe <jkemboe@gmail.com>
2021-12-10 12:23:51 -0500

    Authentication test set up

---
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_users_can_authenticate_using_login_screen()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+        $response->assertNoContent();
+    }
+}
