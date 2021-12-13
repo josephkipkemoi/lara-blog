8d5898a6fd7ade33a09338a6f1c9cf22e894c19c
jkemboe <jkemboe@gmail.com>
2021-12-13 09:54:56 -0500

    Reset Password Auth route

--- routes/auth.php
+++ routes/auth.php
@@ -2,7 +2,14 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest');
+
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])->middleware('guest');
+
+Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+                                ->middleware('guest')
+                                ->name('password.email');
