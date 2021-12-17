c19ff3cdb5f48cce89936eed85f0630264ce2316
jkemboe <jkemboe@gmail.com>
2021-12-17 09:05:00 -0500

    Blog test refactir

--- tests/Feature/Blog/BlogTest.php
+++ tests/Feature/Blog/BlogTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 // use App\Models\Blog;
 
 use App\Models\Blog;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Testing\Fluent\AssertableJson;
@@ -23,9 +24,9 @@ class BlogTest extends TestCase
 
     public function test_can_post_blog()
     {
-        $blog = Blog::factory()->raw();
-
-        $response = $this->post('api/v1/blogs',$blog);
+        $user = User::factory()->create();
+        $blog = Blog::factory()->for($user)->raw();
+        $response = $this->post("api/v1/blogs", $blog);
 
         $response->assertCreated()
                  ->assertJson(fn(AssertableJson $json) => (
