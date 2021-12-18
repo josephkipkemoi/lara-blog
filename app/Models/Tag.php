66dec422fd85e504f09a0d8dabcb00a3bda1b186
jkemboe <jkemboe@gmail.com>
2021-12-17 19:15:35 -0500

    Tag model

---
+++ app/Models/Tag.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['tag','blog_id'];
+
+    protected $casts = ['tag' => 'string'];
+
+    protected $hidden = ['updated_at','created_at'];
+
+
+}
