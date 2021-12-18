9d7221da4edaa14c689d1e0a6ee4a616a2cb922b
jkemboe <jkemboe@gmail.com>
2021-12-18 14:25:22 -0500

    Get tags from post

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateTagRequest;
+use App\Models\Blog;
 use App\Models\Tag;
 use Illuminate\Http\Request;
 
@@ -12,5 +13,10 @@ class TagController extends Controller
     public function __invoke(Tag $tag, CreateTagRequest $request)
     {
         return $tag->create($request->validated());
+    }
+
+    public function show(Tag $tag,$id, Blog $blog)
+    {
+        return $blog->find($id)->tag;
     }
 }
