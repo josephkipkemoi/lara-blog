be2f1f9b97bd11118780c7e232b2d836812f8eb6
jkemboe <jkemboe@gmail.com>
2021-12-19 15:56:10 -0500

    Get single category with blog posts

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateCategoryRequest;
+use App\Models\Blog;
 use App\Models\Category;
 
 class CategoryController extends Controller
@@ -11,5 +12,11 @@ class CategoryController extends Controller
     public function __invoke(Category $category, CreateCategoryRequest $request)
     {
         return $category->create($request->validated());
+    }
+
+    // Get all posts related to category-id
+    public function show($id)
+    {
+        return Category::find($id)->blog;
     }
 }
