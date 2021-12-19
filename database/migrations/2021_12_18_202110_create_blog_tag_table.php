caeacbf540c2cf54e4fcf46f9a3abaac2e48df8b
jkemboe <jkemboe@gmail.com>
2021-12-19 14:22:11 -0500

    Pivot table for blogs and tags

---
+++ database/migrations/2021_12_18_202110_create_blog_tag_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_tags', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('blog_id');
+            $table->foreign('blog_id')
+                  ->references('id')
+                  ->on('blogs')
+                  ->onDelete('cascade');
+
+            $table->unsignedBigInteger('tag_id');
+            $table->foreign('tag_id')
+                        ->references('id')
+                        ->on('tags')
+                        ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_tag');
+    }
+}
