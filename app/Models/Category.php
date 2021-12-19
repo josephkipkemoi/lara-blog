d02b218a2ff06ce5f35e9a75bc00add5122ebf73
jkemboe <jkemboe@gmail.com>
2021-12-19 15:02:59 -0500

    Category model

---
+++ app/Models/Category.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['category'];
+
+    protected $casts = ['category' => 'string'];
+
+    protected $hidden = ['updated_at','created_at'];
+
+}
