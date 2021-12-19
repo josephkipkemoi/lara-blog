e4403d33d429a1f425d9b6e85d38e5de172562a6
jkemboe <jkemboe@gmail.com>
2021-12-19 15:57:36 -0500

    Test for getting posts related to category

--- tests/Feature/Category/CategoryTest.php
+++ tests/Feature/Category/CategoryTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\Category;
 
+use App\Models\Blog;
+use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -22,5 +24,24 @@ class CategoryTest extends TestCase
 
         // Assert status code 201 to show category was created
         $response->assertCreated();
+    }
+
+    public function test_can_get_posts_from_category()
+    {
+        // create blog
+        $blog = Blog::factory()->create();
+
+        // create category
+        $category = Category::factory()->create();
+
+        // attach blog to its category
+        $blog->category()->attach($category);
+
+        // get posts associated with category id
+        $response = $this->get("api/v1/categories/{$category->id}");
+
+        // assert status 200 and posts associated with category are returned
+        $response->assertOk()
+                 ->assertJsonCount(1);
     }
 }
