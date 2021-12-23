cbb89d85fb4af1f6d593cbd77dfd1957b1cc378f
jkemboe <jkemboe@gmail.com>
2021-12-23 12:15:52 -0500

    Category DTO implementatino

--- src/Domains/Category/Http/Controllers/CategoryController.php
+++ src/Domains/Category/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace Domains\Category\Http\Controllers;
 
+use Domains\Category\DTO\CreateCategoryDTO;
 use Domains\Category\Http\Requests\CreateCategoryRequest;
 use Domains\Blog\Models\Blog;
 use Domains\Category\Models\Category;
@@ -11,7 +12,7 @@ class CategoryController extends Controller
     // create/add category
     public function __invoke(Category $category, CreateCategoryRequest $request)
     {
-        return $category->create($request->validated());
+        return $category->create((array) new CreateCategoryDTO(...$request->validated()));
     }
 
     // Get all posts related to category-id
