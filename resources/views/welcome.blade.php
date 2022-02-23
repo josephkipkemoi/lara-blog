208a0dab4d84c89c579ef7d8f5b272abec5be748
josephkipkemoi <jkemboe@gmail.com>
2022-02-23 03:18:35 -0500

    Front End

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,19 +4,52 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Larablog</title>
+    <title>Lara Blog</title>
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Lara Blog</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-    <!-- React root DOM -->
-    <div id="App">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Laravel 9</a>
+      </li>
+     
+     
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+@section('content')
+
+@endsection
+
+<footer class="blog-footer fixed-bottom">
+    <div>
+     <div class="d-flex justify-content-center">
+      <p>Lara Blog</p>
+     </div>
+     <div class="d-flex justify-content-center">
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+     </div>
     </div>
-
-    <!-- React JS -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+</footer>
 
 </body>
 </html>
